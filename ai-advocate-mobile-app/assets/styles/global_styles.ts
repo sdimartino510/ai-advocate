b8df1d467c482c4f8c6360c482b4d0101de9485f
kellyy8 <kellyyu2886@gmail.com>
2024-12-29 23:33:11 -0800

    Created styles folder and translated Figma style guide to code.

---
+++ ai-advocate-mobile-app/assets/styles/global_styles.ts
@@ -0,0 +1,50 @@
+const globalStyles = {
+    colors: {
+        // Primary
+        blue1: "#BAE2FF",
+        blue2: "#E0F2FF",
+        // Secondary
+        buttonBlue1: "C0DAEC",
+        buttonBlue2: "8DBFE4",
+        darkBlue: "0064AE",
+        lightBlue: "F7FCFF",
+        // Supporting
+        black: "#000000",
+        green: "#C5F5C7",
+        grey: "#ECEEF0",
+        lightRed: "#FED9D9",
+        orange: "#FEEAD3",
+        pink: "#DAFED9",
+        purple: "#E3D9FE",
+        red: "#FF0000",
+        yellow: "FFAF37",
+    },
+    fonts: {
+        heading: {
+            fontFamily: "Montserrat_400Regular",
+            fontSize: 26,
+        },
+        subheading: {
+            fontFamily: "Montserrat_700Bold",
+            fontSize: 16,
+        },
+        body: {
+            fontFamily: "Montserrat_400Regular",
+            fontSize: 14,
+        },
+        button1: {
+            fontFamily: "Montserrat_400Regular",
+            fontSize: 14,
+        },
+        button2: {
+            fontFamily: "Montserrat_700Bold",
+            fontSize: 12,
+        },
+        navigation: {
+            fontFamily: "Montserrat_400Regular",
+            fontSize: 10,
+        },
+    },
+}
+
+export default globalStyles;

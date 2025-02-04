f2ed9f47578affcb1bf1b54eea4e910100469cbc
Brittney Cavazos <brittneycavazos@Brittneys-MacBook-Pro.local>
2025-02-04 15:30:41 -0800

    editpage- reposition first button

--- ai-advocate-web-portal/src/app/editpage/page.js
+++ ai-advocate-web-portal/src/app/editpage/page.js
@@ -8,14 +8,14 @@ export default function EditPage() {
         {/* Montserrat is not loading or is being overwritten */
         /* if routing changes, confirm links work */
         }
-        <button className=""><Link href="/">Go Back</Link></button>
-          <div className="">
-              <h1 className="text-4xl">Bill Title</h1>
-              <p className="text-xl font-bold">Bill ID</p>
-              <p>Last updated: 1/10/2025</p>
-          </div>
+        <button className="self-start"><Link href="/">Go Back</Link></button>
+        <div className="">
+          <h1 className="text-4xl">Bill Title</h1>
+          <p className="text-xl font-bold">Bill ID</p>
+          <p>Last updated: 1/10/2025</p>
+        </div>
           {/* {editing sections} */}
-          <button className=""><Link href="/">Back to Home</Link></button>
+        <button className=""><Link href="/">Back to Home</Link></button>
       </div>
     );
   }

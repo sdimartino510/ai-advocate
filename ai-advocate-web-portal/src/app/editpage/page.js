145127ebca61a3d11917f0e44c691094607a213c
Brittney Cavazos <brittneycavazos@Brittneys-MacBook-Pro.local>
2025-02-04 15:27:20 -0800

    billoverview - link back to home

--- ai-advocate-web-portal/src/app/editpage/page.js
+++ ai-advocate-web-portal/src/app/editpage/page.js
@@ -1,18 +1,21 @@
+import Link from "next/link";
 import Header
  from "../components/header";
 export default function EditPage() {
     return (
       <div className="flex flex-col">
         {<Header/>}
-        {/* Montserrat is not loading or is being overwritten */}
-        <button className="">Go Back</button>
+        {/* Montserrat is not loading or is being overwritten */
+        /* if routing changes, confirm links work */
+        }
+        <button className=""><Link href="/">Go Back</Link></button>
           <div className="">
               <h1 className="text-4xl">Bill Title</h1>
               <p className="text-xl font-bold">Bill ID</p>
               <p>Last updated: 1/10/2025</p>
           </div>
           {/* {editing sections} */}
-          <button className="">Back to Home</button>
+          <button className=""><Link href="/">Back to Home</Link></button>
       </div>
     );
   }

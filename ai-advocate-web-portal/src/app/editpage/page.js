5e3879a2641bc2898ec7de07501b0c61cbfdedf3
Brittney Cavazos <brittneycavazos@Brittneys-MacBook-Pro.local>
2025-02-09 18:09:08 -0800

    work on editpage title alignment, top button margin

--- ai-advocate-web-portal/src/app/editpage/page.js
+++ ai-advocate-web-portal/src/app/editpage/page.js
@@ -13,13 +13,13 @@ export default function EditPage() {
           title:'Pros and Cons'}}
 
     return (
-      <div className="flex flex-col gap-9">
+      <div className="flex flex-col gap-9 items-center">
         {<Header/>}
         {/* Montserrat is not loading or is being overwritten */
         /* if routing changes, confirm links work */
         }
-        <button className="self-start"><Link href="/">Go Back</Link></button>
-        <div className="lg:px-44 p-5">
+        <button className="self-start ml-5"><Link href="/">Go Back</Link></button>
+        <div className="lg:px-40 p-5 lg:w-[1200]">
           <h1 className="text-4xl">Bill Title</h1>
           <p className="text-xl font-bold">Bill ID</p>
           <p>Last updated: 1/10/2025</p>

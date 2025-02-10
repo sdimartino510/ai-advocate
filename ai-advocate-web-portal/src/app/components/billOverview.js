473d716dac8d354c33482ad41ad1fe37b36e0ae5
Brittney Cavazos <brittneycavazos@Brittneys-MacBook-Pro.local>
2025-02-10 12:12:51 -0800

    billOverview: add state to alert updated bills

--- ai-advocate-web-portal/src/app/components/billOverview.js
+++ ai-advocate-web-portal/src/app/components/billOverview.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import { useState } from "react";
 export default function BillOverview() {
+
+  const [updated, setUpdated] = useState(false)
+
   return (
     <div className="grid grid-cols-4 border border-grayBlue rounded-lg shadow-md shadow-black/25 bg-white w-full lg:w-[960px] p-5 gap-3">
         <div className="col-span-4 row-span-3">
             {/* Montserrat is not loading or is being overwritten */}
             <h2 className="text-4xl">Bill Title</h2>
             <h3 className="text-xl font-bold">Bill ID</h3>
-            <p>Last updated: 1/10/2025</p>
+            <p className={`${updated? 'text-red' : 'text-black'}`}>Last updated: 1/10/2025</p>
             <div className="flex gap-4 pt-4">
                 <p>Verified:</p>
                 <p>Pros:</p>
